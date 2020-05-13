fb2fe18fb717520e6ea42e40485ec658f27dda2a
James Stenger <jstenger@csil-01.cs.ucsb.edu>
2020-05-12 20:28:48 -0700

    CSIL compatible

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -3,37 +3,27 @@ import sys
 import time
 
 
-def train(training, smoothing_factors):
+def train(training, alpha):
 
     training_data = parse(training)
     p_count, n_count = 0, 0
-
     p_word_dict, n_word_dict = {}, {}
     p_pair_dict, n_pair_dict = {}, {}
-    p_triple_dict, n_triple_dict = {}, {}
-
     for review in training_data:
-        words = set(review[0])
-
         pairs = []
-        triples = []
         for i in range(len(review[0]) - 1):
             pairs.append(review[0][i] + " " + review[0][i + 1])
 
-        for i in range(len(review[0]) - 2):
-            triples.append(review[0][i] + " " + review[0][i + 1] + " " + review[0][i + 2])
-
+        words = set(review[0])
         if review[1] == 0:
             n_count += 1
             word_dict = n_word_dict
             pair_dict = n_pair_dict
-            triple_dict = n_triple_dict
 
         else:
             p_count += 1
             word_dict = p_word_dict
             pair_dict = p_pair_dict
-            triple_dict = p_triple_dict
 
         for word in words:
             if word in word_dict:
@@ -47,101 +37,68 @@ def train(training, smoothing_factors):
             else:
                 pair_dict[pair] = 1
 
-        for triple in triples:
-            if triple in triple_dict:
-                triple_dict[triple] += 1
-            else:
-                triple_dict[triple] = 1
-
     p_word_sum = sum(p_word_dict.values())
     n_word_sum = sum(n_word_dict.values())
+
     vocabulary = set().union(p_word_dict.keys(), n_word_dict.keys())
     p_word_dict.update([(word, 0) for word in vocabulary - set(p_word_dict.keys())])
     n_word_dict.update([(word, 0) for word in vocabulary - set(n_word_dict.keys())])
 
-
     p_pair_sum = sum(p_pair_dict.values())
     n_pair_sum = sum(n_pair_dict.values())
+
     pair_vocabulary = set().union(p_pair_dict.keys(), n_pair_dict.keys())
-    p_pair_dict.update([(pair, 0) for pair in pair_vocabulary - set(p_pair_dict.keys())])
-    n_pair_dict.update([(pair, 0) for pair in pair_vocabulary - set(n_pair_dict.keys())])
-
-    p_triple_sum = sum(p_triple_dict.values())
-    n_triple_sum = sum(n_triple_dict.values())
-    triple_vocabulary = set().union(p_triple_dict.keys(), n_triple_dict.keys())
-    p_triple_dict.update([(triple, 0) for triple in triple_vocabulary - set(p_triple_dict.keys())])
-    n_triple_dict.update([(triple, 0) for triple in triple_vocabulary - set(n_triple_dict.keys())])
+    p_pair_dict.update([(word, 0) for word in pair_vocabulary - set(p_pair_dict.keys())])
+    n_pair_dict.update([(word, 0) for word in pair_vocabulary - set(n_pair_dict.keys())])
 
 
-    p_positive = {word: math.log((p_word_dict[word] + smoothing_factors[0]) / (p_word_sum + smoothing_factors[0] * len(vocabulary)))
-                       for word in vocabulary}
 
-    p_negative = {word: math.log((n_word_dict[word] + smoothing_factors[0]) / (n_word_sum + smoothing_factors[0] * len(vocabulary)))
-                       for word in vocabulary}
 
-    word_probabilities = (p_negative, p_positive)
 
-    p_positive = {pair: math.log((p_pair_dict[pair] + smoothing_factors[1]) / (p_pair_sum + smoothing_factors[1] * len(pair_vocabulary)))
+
+    # p_positive = {word: math.log((p_word_dict[word] + alpha) / (p_word_sum + alpha * len(vocabulary)))
+    #                    for word in vocabulary}
+    #
+    # p_negative = {word: math.log((n_word_dict[word] + alpha) / (n_word_sum + alpha * len(vocabulary)))
+    #                    for word in vocabulary}
+
+    p_positive = {pair: math.log((p_pair_dict[pair] + alpha) / (p_pair_sum + alpha * len(pair_vocabulary)))
                   for pair in pair_vocabulary}
 
-    p_negative = {pair: math.log((n_pair_dict[pair] + smoothing_factors[1]) / (n_pair_sum + smoothing_factors[1] * len(pair_vocabulary)))
+    p_negative = {pair: math.log((n_pair_dict[pair] + alpha) / (n_pair_sum + alpha * len(pair_vocabulary)))
                   for pair in pair_vocabulary}
 
-    pair_probabilities = (p_negative, p_positive)
-
-    p_positive = {triple: math.log((p_triple_dict[triple] + smoothing_factors[2]) / (p_triple_sum + smoothing_factors[2] * len(triple_vocabulary)))
-                  for triple in triple_vocabulary}
-
-    p_negative = {triple: math.log((n_triple_dict[triple] + smoothing_factors[2]) / (n_triple_sum + smoothing_factors[2] * len(triple_vocabulary)))
-                  for triple in triple_vocabulary}
-
-    triple_probabilities = (p_negative, p_positive)
+    return p_positive, p_negative, vocabulary, pair_vocabulary
 
 
-    probabilities = (word_probabilities, pair_probabilities, triple_probabilities)
-
-    vocabularies = (vocabulary, pair_vocabulary, triple_vocabulary)
-
-    return probabilities, vocabularies
-
-
-def test(testing, probabilities, vocabularies, weights):
+def test(testing, p_positive, p_negative, vocabulary, pair_vocabulary):
 
     testing_data = parse(testing)
 
+    labels = ""
+
     linecount, correctcount = 0, 0
     for review in testing_data:
-        word_probability_pos, word_probability_neg = 0, 0
-        for word in review[0]:
-            if word in vocabularies[0]:
-                word_probability_pos += probabilities[0][1][word]
-                word_probability_neg += probabilities[0][0][word]
+        positive, negative = 0, 0
+        # for word in review[0]:
+        #     if word in vocabulary:
+        #         positive += p_positive[word]
+        #         negative += p_negative[word]
 
-        pair_probability_pos, pair_probability_neg = 0, 0
         for i in range(len(review[0]) - 1):
             pair = review[0][i] + " " + review[0][i + 1]
-            if pair in vocabularies[1]:
-                pair_probability_pos += probabilities[1][1][pair]
-                pair_probability_neg += probabilities[1][0][pair]
+            if pair in pair_vocabulary:
+                positive += p_positive[pair]
+                negative += p_negative[pair]
 
-        triple_probability_pos, triple_probability_neg = 0, 0
-        for i in range(len(review[0]) - 2):
-            triple = review[0][i] + " " + review[0][i + 1] + " " + review[0][i + 2]
-            if triple in vocabularies[2]:
-                triple_probability_pos += probabilities[2][1][triple]
-                triple_probability_neg += probabilities[2][0][triple]
-
-        probability_pos = weights[0] * word_probability_pos + weights[1] * pair_probability_pos + weights[2] * triple_probability_pos
-        probability_neg = weights[0] * word_probability_neg + weights[1] * pair_probability_neg + weights[2] * triple_probability_neg
-
-
-        if probability_neg > probability_pos:
-
+        if negative > positive:
+            labels += "0\n"
             if review[1] == 0:
                 correctcount += 1
             # else:
                 # print("False negative: " + " ".join(review[0]))
         else:
+            labels += "1\n"
             if review[1] == 1:
                 correctcount += 1
             # else:
@@ -149,8 +106,10 @@ def test(testing, probabilities, vocabularies, weights):
 
         linecount += 1
 
+
     accuracy = correctcount / linecount
-    return accuracy
+    labels = labels[:-1]
+    return accuracy, labels
 
 
 def parse(filename):
@@ -167,20 +126,21 @@ def parse(filename):
 
 
 def main():
-    smoothing_factors = [2, 0.6, .5]
-    weights = [3, 10, 10]
-
     t = time.perf_counter()
-    probabilities, vocabularies = train(sys.argv[1], smoothing_factors)
+    p_positive, p_negative, vocabulary, pair_vocabulary = train(sys.argv[1], 1)
     training_time = time.perf_counter() - t
 
     t = time.perf_counter()
-    testing_accuracy = test(sys.argv[2], probabilities, vocabularies, weights)
+    testing_accuracy, testing_labels = test(sys.argv[2], p_positive, p_negative, vocabulary, pair_vocabulary)
     testing_time = time.perf_counter() - t
 
+    print(testing_labels)
     print(str(round(training_time)) + " seconds (training)")
     print(str(round(testing_time)) + " seconds (testing)")
 
-    training_accuracy = test(sys.argv[2], probabilities, vocabularies, weights)
+    training_accuracy, training_labels = test(sys.argv[1], p_positive, p_negative, vocabulary, pair_vocabulary)
     print(str(round(training_accuracy, 3)) + " (training)")
     print(str(round(testing_accuracy, 3)) + " (testing)")
+
+
+main()
